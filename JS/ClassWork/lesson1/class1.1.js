896f6adcc7a059e1c978e786c4b3853025944557
YuliyaApolonova <yuliyaapl@mail.ru>
2017-03-27 22:31:00 +0300

    Revert "fuckup"
    This reverts commit c2e63c5fb001a45a2a351a26bc768c2f77bc5942.

---
+++ JS/ClassWork/lesson1/class1.1.js
@@ -0,0 +1,19 @@
+var user={};
+user.name="Vasya";
+user.surname="Petrov";
+user.name="Sergey";
+delete user.name;
+
+function IsEmpty(obj)
+{
+	for(var key in obj){ return false;}
+		return true;
+		
+}
+var res;
+res=IsEmpty(user);
+alert (res);
+var menu={};
+res=IsEmpty(menu);
+alert (res);
+
